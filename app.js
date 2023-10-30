b45949ccf603040c8bbd4e4ee08d3b5b207d7699
ambu360 <75344498+ambu360@users.noreply.github.com>
2023-10-30 14:14:57 -0400

    added env

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config()
 
+const mongoose = require('mongoose')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+mongoose.set('strictQuery',false)
+const mongoDb = process.env.mongoURL
+
+main().catch(err=>console.log(err))
+async function main(){
+  await mongoose.connect(mongoDb);
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
